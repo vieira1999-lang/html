5324a4c1ac596abc0964a9db1b9f2dd158989d60
vieira1999-lang <developer_vieira@proton.me>
2026-05-03 21:51:01 -0300

    Front-End: Criando descrição do usuário. Adicionando fonst geist-mono/regular

--- template-vanilla/js/components/web-components-svg.js
+++ template-vanilla/js/components/web-components-svg.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     class LinhaGuia extends HTMLElement {
         connectedCallback() { // metodo connectedCallback() é chamada todo vez que o navehador identificar sua "tag" 
             this.innerHTML = `  <svg width="100%" height="100%" style="position: absolute; pointer-events: none;">
-                                <line x1="0" y1="50%" x2="100%" y2="50%" stroke="#444" stroke-dasharray="4" />
+                                <line x1="5%" y1="0%" x2="100%" y2="0%" stroke="#8b8b8b" stroke-dasharray="4" />
                                 </svg>`; // Conteudo usado dentro "tag"
         }
     }
